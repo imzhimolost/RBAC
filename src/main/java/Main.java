709b61d85aad7d93fdc17f6a2486a3f1629aecb9
imzhimolost <shinkarenkokseniainfo@gmail.com>
2026-02-17 12:42:24 +0700

    Create class Role

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import model.User;
 import model.Permission;
+import model.Role;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Тест первого пункта");
+        System.out.println("-----Тест первого пункта-----\n");
 
         test("Верные данные", "ivan2008", "Иван Иванов Иванович","ivan2008@example.com" );
 
@@ -20,10 +21,23 @@ public class Main {
         test("Нет собачки", "ivan2008", "Иван Иванов Иванович","ivan2008example.com" );
         test("Нет точки", "ivan2008", "Иван Иванов Иванович","ivan2008@examplecom" );
 
-        System.out.println("Тест второго пункта");
+        System.out.println("\n-----Тест второго пункта-----\n");
 
         Permission permission = new Permission("WRITE", "ivan2008", "write some data");
         System.out.println(permission.format());
+
+        System.out.println("\n-----Тест третьего пункта-----\n");
+
+        Role role = new Role("ivan2008", "description");
+        Role role1 = new Role("stepan2021", "stepan brawlstars");
+        Permission permission1 = new Permission("Read", "stepan2021", "read some books");
+        role.addPermission(permission);
+        role.addPermission(permission1);
+        role1.addPermission(permission1);
+        System.out.println(role.equals(role1));
+        System.out.println(role.equals(role));
+        System.out.println(role.toString());
+        System.out.println(role.format());
     }
 
     public static void test(String testname, String username, String fullName, String email){
---
+++ src/main/java/model/Role.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+public class Role {
+    String id;
+    String name;
+    String description;
+    Set<Permission> permissions;
+
+    public Role(String username, String userdescription){
+        id = "role_" + UUID.randomUUID().toString();
+        name = username;
+        description = userdescription;
+        permissions = new HashSet<>();
+    }
+
+    public void addPermission(Permission permission){
+        permissions.add(permission);
+    }
+
+    public void removePermission(Permission permission){
+        permissions.remove(permission);
+    }
+
+    public boolean hasPermission(Permission permission){
+        return permissions.contains(permission);
+    }
+
+    public boolean hasPermission(String permissionName, String resource){
+        for(Permission item : permissions){
+            if(item.matches(permissionName, resource)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id.equals(role.id);
+    }
+
+    @Override
+    public int hashCode(){
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString(){
+        String result = "Role: {id: '" + id + "', name: '" + name + "', description: '" + description + "', permissions: \n";
+        for (Permission item : permissions){
+            result = result + " - " + item.format() + "\n";
+        }
+        return result;
+    }
+
+    public String format(){
+        String result = "Role: " + name + " [ID: " + id + "] \nDescription: " + description + " \nPermissions (" + permissions.size() + "):\n";
+        for(Permission item : permissions){
+            result = result + " - " + item.format() + "\n";
+        }
+        return result;
+    }
+}
