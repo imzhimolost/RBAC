9548b1bf9e7eb4b4df69b1838f05018e769e0885
imzhimolost <shinkarenkokseniainfo@gmail.com>
2026-02-24 21:13:49 +0700

    Create AssigmentFilters

---
+++ src/main/java/filters/AssignmentFilter.java
@@ -0,0 +1,16 @@
+package filters;
+
+import model.RoleAssignment;
+
+@FunctionalInterface
+public interface AssignmentFilter {
+    boolean test(RoleAssignment assignment);
+
+    default AssignmentFilter and(AssignmentFilter other){
+        return assignment -> this.test(assignment) && other.test(assignment);
+    }
+
+    default AssignmentFilter or(AssignmentFilter other) {
+        return assignment -> this.test(assignment) || other.test(assignment);
+    }
+}
